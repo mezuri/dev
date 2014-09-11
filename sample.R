efa4640b1c2bbeff8eedbde62e583699708eb29f
mezuri <kipf@berkeley.edu>
2014-09-10 22:10:21 -0700

    updated jar

--- sample.R
+++ sample.R
@@ -1,4 +1,3 @@
-write("script started", file = "/tmp/started_flag")
 Sys.sleep(60)
 write("blob output", file = "/tmp/outputs/blob.output")
 write("successfully completed", file = "/tmp/success_flag")
